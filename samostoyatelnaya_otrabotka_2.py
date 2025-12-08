b21d725b200a0bd0aa0be6c4a39376e063ee5e30
saretskywalker <138796350+saretskywalker@users.noreply.github.com>
2025-12-08 10:33:24 +0300

    Update samostoyatelnaya_otrabotka_2.py

--- samostoyatelnaya_otrabotka_2.py
+++ samostoyatelnaya_otrabotka_2.py
@@ -1,8 +1,9 @@
 product = 1
 while True:
     number = int(input("Введите число "))
-    if number < 0:
-        break
+    
     if number > 0:
         product *= number
+    if number < 0:
+        break
 print (product)
